950cb562b1ada834ed7675d425d174d0932bfc6d
Jamie <jamie.al@outlook.de>
2026-01-27 14:22:14 +0100

    Kleine anderung und änderung zu php

--- aufgaben.php
+++ aufgaben.php
@@ -4,21 +4,30 @@
     <meta charset="UTF-8">
     <title>Aufgaben</title>
     <link href="styles/styelsaufgaben.css" rel="stylesheet"/>
+    <link href="aufgaben.php" rel="" />
 </head>
 <body>
 <h1>Aufgaben</h1>
 
 
 <div class="tage">
-    <h2>27.9.26</h2>
+    <h2>
+        <?php
+        echo date("d.m.Y");
+        ?>
+    </h2>
     <p class="aufgaben">Eine Aufgabe</p>
     <p class="aufgaben"> aufgabe </p>
+</div>
+<div class="tag">
     <h2>28.9.26</h2>
     <p class="aufgaben">Andere Aufgabe</p>
 </div>
 
 
+<a href="index.php">Hauptseite</a>
+<p></p>
+<a href="test.php">test</a>
 
-<a href="index.html">Hauptseite</a>
 </body>
 </html>
