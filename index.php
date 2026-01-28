02c1c094372debe74d83ed7b3972247a41a1c439
Jamie <jamie.al@outlook.de>
2026-01-28 08:42:39 +0100

    kleines test formular

---
+++ index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <title>Schulorga</title>
+    <link href="styles/styles.css" rel="stylesheet"/>
+</head>
+<body>
+<h1>Schulorga</h1>
+<div class="stundenplan">
+    <div class="tag">
+        <h2>Mo</h2>
+        <hr>
+        <p>F1</p>
+        <p>F2</p>
+        <p>F3</p>
+        <p>F4</p>
+        <p>F5</p>
+        <p>F6</p>
+
+    </div>
+    <div class="tag">
+        <h2>Di</h2>
+        <hr>
+        <p>F1</p>
+        <p>F2</p>
+        <p>F3</p>
+        <p>F4</p>
+        <p>F5</p>
+        <p>F6</p>
+    </div>
+    <div class="tag">
+        <h2>Mi</h2>
+        <hr>
+        <p>F1</p>
+        <p>F2</p>
+        <p>F3</p>
+        <p>F4</p>
+        <p>F5</p>
+        <p>F6</p>
+
+    </div>
+    <div class="tag">
+        <h2>Do</h2>
+        <hr>
+        <p>F1</p>
+        <p>F2</p>
+        <p>F3</p>
+        <p>F4</p>
+        <p>F5</p>
+        <p>F6</p>
+
+    </div>
+    <div class="tag">
+        <h2>Fr</h2>
+        <hr>
+        <p>F1</p>
+        <p>F2</p>
+        <p>F3</p>
+        <p>F4</p>
+        <p>F5</p>
+        <p>F6</p>
+
+    </div>
+</div>
+<a href="aufgaben.php">Aufgabenseite</a>
+
+</body>
+</html>
---
+++ test.php
@@ -0,0 +1,11 @@
+<form id=" Text ">
+    <h1> Formular</h1>
+    <label for="vorname">Vorname</label>
+    <input name = vorname>
+    <button type="submit"> Abfeueren </button>
+</form>
+<?php
+
+     echo " Dein Vornmae ist " .$_GET["vorname"];
+?>
+<a href="index.php"> Startseite </a>
