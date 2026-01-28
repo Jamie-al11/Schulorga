0d0b35158b19424b2a93bd4795dc0035bfc121f4
Jamie <jamie.al@outlook.de>
2026-01-28 08:44:51 +0100

    kleines test formular

--- test.php
+++ test.php
@@ -1,11 +1,8 @@
 <form id=" Text ">
     <h1> Formular</h1>
     <label for="vorname">Vorname</label>
-    <input name = vorname>
+    <input name = vorname />
     <button type="submit"> Abfeueren </button>
 </form>
-<?php
 
-     echo " Dein Vornmae ist " .$_GET["vorname"];
-?>
 <a href="index.php"> Startseite </a>
