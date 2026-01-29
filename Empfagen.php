143596d3ab78fba805e8a299dddd93753d5e9c14
Jamie <jamie.al@outlook.de>
2026-01-29 14:40:15 +0100

    kleiner Fehler gefixt

--- Empfagen.php
+++ Empfagen.php
@@ -1,27 +1,29 @@
 <?php
-$mof1 = $_POST['mof1'];
-$mof2 = $_POST['mof2'];
-$mof3 = $_POST['mof3'];
-$mof4 = $_POST['mof4'];
-$mof5 = $_POST['mof5'];
-$mof6 = $_POST['mof6'];
+$mof1 = $_POST['mof0'];
+$mof2 = $_POST['mof1'];
+$mof3 = $_POST['mof2'];
+$mof4 = $_POST['mof3'];
+$mof5 = $_POST['mof4'];
+$mof6 = $_POST['mof5'];
 
 
+if ($mof1 !== " ")
+    file_put_contents('test.txt', $mof1."\n");
 
-file_put_contents('test.txt', $mof1."\n");
-file_put_contents('test.txt', $mof2."\n", FILE_APPEND);
-file_put_contents('test.txt', $mof3."\n", FILE_APPEND);
-file_put_contents('test.txt', $mof4."\n", FILE_APPEND);
-file_put_contents('test.txt', $mof5."\n", FILE_APPEND);
-file_put_contents('test.txt', $mof6."\n", FILE_APPEND);
+if ($mof2 !== " ")
+    file_put_contents('test.txt', $mof2."\n", FILE_APPEND);
 
-//for ($i = 1;$i < 7;$i++){
-//    $mof1 = $_POST['mof1'];
-//    file_put_contents('test.txt', $mof($i)."\n");
-//
-//}
-// das als schleife machen muss iwi gehen
+if ($mof3 !== " ")
+    file_put_contents('test.txt', $mof3."\n", FILE_APPEND);
 
+if ($mof4 !== " ")
+    file_put_contents('test.txt', $mof4."\n", FILE_APPEND);
+
+if ($mof5 !== " ")
+    file_put_contents('test.txt', $mof5."\n", FILE_APPEND);
+
+if ($mof6 !== " ")
+    file_put_contents('test.txt', $mof6."\n", FILE_APPEND);
 
 
 header("Location: http://localhost/Schulorga/");
