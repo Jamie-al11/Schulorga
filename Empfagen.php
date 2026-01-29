a22bce0a5629d618191981f3fc41c7dc3aea9e89
Jamie <jamie.al@outlook.de>
2026-01-29 11:13:59 +0100

    alle fächer am monatg funkt jetzt in besser

--- Empfagen.php
+++ Empfagen.php
@@ -1,15 +1,26 @@
 <?php
-
-$mof0 = $_POST["mof"];
-$mof1 = $_POST['mof'];
-
-
-
-file_put_contents('test.txt', $mof0."\n");
-file_put_contents('test.txt', $mof1."\n", FILE_APPEND);
+$mof1 = $_POST['mof1'];
+$mof2 = $_POST['mof2'];
+$mof3 = $_POST['mof3'];
+$mof4 = $_POST['mof4'];
+$mof5 = $_POST['mof5'];
+$mof6 = $_POST['mof6'];
 
 
 
+file_put_contents('test.txt', $mof1."\n");
+file_put_contents('test.txt', $mof2."\n", FILE_APPEND);
+file_put_contents('test.txt', $mof3."\n", FILE_APPEND);
+file_put_contents('test.txt', $mof4."\n", FILE_APPEND);
+file_put_contents('test.txt', $mof5."\n", FILE_APPEND);
+file_put_contents('test.txt', $mof6."\n", FILE_APPEND);
+
+//for ($i = 1;$i < 7;$i++){
+//    $mof1 = $_POST['mof1'];
+//    file_put_contents('test.txt', $mof($i)."\n");
+//
+//}
+// das als schleife machen muss iwi gehen
 
 
 
--- formularst.php
+++ formularst.php
@@ -1,14 +1,13 @@
 
-<form method="post" action="formularst.php">
+<form method="post" action="empfagen.php">
     <h1> Formular</h1>
     <?php
-    for ($i = 0;$i < 6;$i++) {
+    for ($i = 1;$i < 7;$i++) {
         echo "<label> $i </label>";
-        echo "<input type='text' name='mof[$i]'>";
+        echo "<input type='text' name='mof$i'>";
         echo "<p></p>";
-
     }
     ?>
     <button type="submit"> Fertig </button>
 </form>
-<a href="index.php"> Startseite </a>
+<a href="index.php">Startseite</a>
