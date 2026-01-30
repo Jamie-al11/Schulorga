b42125195166576c17eb7257f27ad83f72131bd3
Jamie <jamie.al@outlook.de>
2026-01-30 16:15:00 +0100

    Formular in eine foreach schleife gepackt

--- formularst.php
+++ formularst.php
@@ -8,99 +8,28 @@
 <h1>Schulorga</h1>
 
 <div class="stundenplan">
-    <div class="tag">
-        <h2>Mo</h2>
-        <hr>
-        <?php
+    <?php
+        foreach(['mo','di','mi','do','fr'] as $wochentag){
+            echo ('<div class="tag">');
+            echo ("<h2>$wochentag</h2>");
+            echo ("<hr>");
         $x = 6; // Anzahl der Stunden die ein fach abfragen
         $json = file_get_contents("test.txt");
         $plan = json_decode($json, true);
-        $mo = $plan["mof"];
-        $n = sizeof($mo);
+        $tag = $plan[$wochentag."f"];
+        $n = sizeof($tag);
         for ($i = 0;$i < $x;$i++) {
             echo "<input type='hidden' name='anzahl'  value = $x >";
             if ($i   < $n ) {
                 echo "<p>
-                    <input type='text' name='mof[]' value=$mo[$i]></p>";}
+                    <input type='text' name='{$wochentag}f[]' value=$tag[$i]></p>";}
             else{
                echo "<p>
-                    <input type='text' name='mof[]'></p>";
+                    <input type='text' name='$wochentag.f[]'></p>";
                     }}
+        echo "</div>";}
         ?>
-    </div>
-  <div class="tag">
-      <h2>Di</h2>
-      <hr>
-      <?php
-      $json = file_get_contents("test.txt");
-      $plan = json_decode($json, true);
-      $di = $plan["dif"];
-      $n = sizeof($di);
-      for ($i = 0;$i < $x;$i++) {
-          if ($i   < $n ) {
-              echo "<p>
-                    <input type='text' name='dif[]' value=$di[$i]></p>";}
-          else{
-              echo "<p>
-                    <input type='text' name='dif[]'></p>";
-          }}
-      ?>
-  </div>
-  <div class="tag">
-      <h2>Mi</h2>
-      <hr>
-      <?php
-      $json = file_get_contents("test.txt");
-      $plan = json_decode($json, true);
-      $mi = $plan["mif"];
-      $n = sizeof($mi);
-      for ($i = 0;$i < $x;$i++) {
-          if ($i   < $n ) {
-              echo "<p>
-                    <input type='text' name='mif[]' value=$mi[$i]></p>";}
-          else{
-              echo "<p>
-                    <input type='text' name='mif[]'></p>";
-          }}
-      ?>
-  </div>
-  <div class="tag">
-      <h2>Do</h2>
-      <hr>
-      <?php
-      $json = file_get_contents("test.txt");
-      $plan = json_decode($json, true);
-      $do = $plan["dof"];
-      $n = sizeof($do);
-      for ($i = 0;$i < $x;$i++) {
-          if ($i   < $n ) {
-              echo "<p>
-                    <input type='text' name='dof[]' value=$do[$i]></p>";}
-          else{
-              echo "<p>
-                    <input type='text' name='dof[]'></p>";
-          }}
-      ?>
-  </div>
-  <div class="tag">
-      <h2>Fr</h2>
-      <hr>
-      <?php
-      $json = file_get_contents("test.txt");
-      $plan = json_decode($json, true);
-      $fr = $plan["frf"];
-      $n = sizeof($fr);
-      for ($i = 0;$i < $x;$i++) {
-          if ($i   < $n ) {
-              echo "<p>
-                    <input type='text' name='frf[]' value=$fr[$i]></p>";}
-          else{
-              echo "<p>
-                    <input type='text' name='frf[]'></p>";
-          }}
-      ?>
 
-  </div>
 </div>
     <button type="submit"> Fertig </button>
 </form>
