e63eeb0c1a801568e4c64ea76987f6d87e1bdd4a
Jamie <jamie.al@outlook.de>
2026-01-30 15:47:04 +0100

    Index ist nun eine foreach schleife

--- index.php
+++ index.php
@@ -8,74 +8,76 @@
 <body>
 <h1>Schulorga</h1>
 <div class="stundenplan">
-    <div class="tag">
-        <h2>Mo</h2>
-        <hr>
-        <?php
+    <?php
+        foreach(['mo','di','mi','do','fr'] as $wochentag){
+            echo '<div class="tag">';
+            echo "<h2>$wochentag</h2>";
+            echo "<hr>";
         $json = file_get_contents("test.txt");
         $plan = json_decode($json, true);
-        $mo = $plan["mof"];
-        $mn =sizeof($mo);
-        for ($i = 0;$i < $mn;$i++) {
-            echo "<p>$mo[$i]</p>";
+        $wochentag = $plan[$wochentag."f"];
+        $n =sizeof($wochentag);
+        for ($i = 0;$i < $n;$i++) {
+            echo "<p>$wochentag[$i]</p>";
         }
-        ?>
-    </div>
-    <div class="tag">
-        <h2>Di</h2>
-        <hr>
-        <?php
-        $json = file_get_contents("test.txt");
-        $plan = json_decode($json, true);
-        $mo = $plan["dif"];
-        $mn =sizeof($mo);
-        for ($i = 0;$i < $mn;$i++) {
-            echo "<p>$mo[$i]</p>";
-        }
-        ?>
-    </div>
-    <div class="tag">
-        <h2>Mi</h2>
-        <hr>
-        <?php
-        $json = file_get_contents("test.txt");
-        $plan = json_decode($json, true);
-        $mo = $plan["mif"];
-        $mn =sizeof($mo);
-        for ($i = 0;$i < $mn;$i++) {
-            echo "<p>$mo[$i]</p>";
-        }
-        ?>
 
-    </div>
-    <div class="tag">
-        <h2>Do</h2>
-        <hr>
-        <?php
-        $json = file_get_contents("test.txt");
-        $plan = json_decode($json, true);
-        $mo = $plan["dof"];
-        $mn =sizeof($mo);
-        for ($i = 0;$i < $mn;$i++) {
-            echo "<p>$mo[$i]</p>";
+    echo ("</div>");
         }
-        ?>
-
-    </div>
-    <div class="tag">
-        <h2>Fr</h2>
-        <hr>
-        <?php
-        $json = file_get_contents("test.txt");
-        $plan = json_decode($json, true);
-        $mo = $plan["frf"];
-        $mn =sizeof($mo);
-        for ($i = 0;$i < $mn;$i++) {
-            echo "<p>$mo[$i]</p>";
-        }
-        ?>
-
-    </div>
+    ?>
+<!--    <div class="tag">-->
+<!--        <h2>Di</h2>-->
+<!--        <hr>-->
+<!--        --><?php
+//        $json = file_get_contents("test.txt");
+//        $plan = json_decode($json, true);
+//        $mo = $plan["dif"];
+//        $mn =sizeof($mo);
+//        for ($i = 0;$i < $mn;$i++) {
+//            echo "<p>$mo[$i]</p>";
+//        }
+//        ?>
+<!--    </div>-->
+<!--    <div class="tag">-->
+<!--        <h2>Mi</h2>-->
+<!--        <hr>-->
+<!--        --><?php
+//        $json = file_get_contents("test.txt");
+//        $plan = json_decode($json, true);
+//        $mo = $plan["mif"];
+//        $mn =sizeof($mo);
+//        for ($i = 0;$i < $mn;$i++) {
+//            echo "<p>$mo[$i]</p>";
+//        }
+//        ?>
+<!---->
+<!--    </div>-->
+<!--    <div class="tag">-->
+<!--        <h2>Do</h2>-->
+<!--        <hr>-->
+<!--        --><?php
+//        $json = file_get_contents("test.txt");
+//        $plan = json_decode($json, true);
+//        $mo = $plan["dof"];
+//        $mn =sizeof($mo);
+//        for ($i = 0;$i < $mn;$i++) {
+//            echo "<p>$mo[$i]</p>";
+//        }
+//        ?>
+<!---->
+<!--    </div>-->
+<!--    <div class="tag">-->
+<!--        <h2>Fr</h2>-->
+<!--        <hr>-->
+<!--        --><?php
+//        $json = file_get_contents("test.txt");
+//        $plan = json_decode($json, true);
+//        $mo = $plan["frf"];
+//        $mn =sizeof($mo);
+//        for ($i = 0;$i < $mn;$i++) {
+//            echo "<p>$mo[$i]</p>";
+//        }
+//        ?>
+<!--    </div>-->
 </div>
 <a href="aufgaben.php">Aufgabenseite</a>
 <a href="formularst.php" >Bearbeiten</a>
