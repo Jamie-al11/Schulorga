7557b297657134cfe1e3122b592b36e80c44ae6f
Jamie <jamie.al@outlook.de>
2026-01-30 15:58:34 +0100

    ausgeklammerten code gelöscht

--- index.php
+++ index.php
@@ -24,60 +24,7 @@
     echo ("</div>");
         }
     ?>
-<!--    <div class="tag">-->
-<!--        <h2>Di</h2>-->
-<!--        <hr>-->
-<!--        --><?php
-//        $json = file_get_contents("test.txt");
-//        $plan = json_decode($json, true);
-//        $mo = $plan["dif"];
-//        $mn =sizeof($mo);
-//        for ($i = 0;$i < $mn;$i++) {
-//            echo "<p>$mo[$i]</p>";
-//        }
-//        ?>
-<!--    </div>-->
-<!--    <div class="tag">-->
-<!--        <h2>Mi</h2>-->
-<!--        <hr>-->
-<!--        --><?php
-//        $json = file_get_contents("test.txt");
-//        $plan = json_decode($json, true);
-//        $mo = $plan["mif"];
-//        $mn =sizeof($mo);
-//        for ($i = 0;$i < $mn;$i++) {
-//            echo "<p>$mo[$i]</p>";
-//        }
-//        ?>
-<!---->
-<!--    </div>-->
-<!--    <div class="tag">-->
-<!--        <h2>Do</h2>-->
-<!--        <hr>-->
-<!--        --><?php
-//        $json = file_get_contents("test.txt");
-//        $plan = json_decode($json, true);
-//        $mo = $plan["dof"];
-//        $mn =sizeof($mo);
-//        for ($i = 0;$i < $mn;$i++) {
-//            echo "<p>$mo[$i]</p>";
-//        }
-//        ?>
-<!---->
-<!--    </div>-->
-<!--    <div class="tag">-->
-<!--        <h2>Fr</h2>-->
-<!--        <hr>-->
-<!--        --><?php
-//        $json = file_get_contents("test.txt");
-//        $plan = json_decode($json, true);
-//        $mo = $plan["frf"];
-//        $mn =sizeof($mo);
-//        for ($i = 0;$i < $mn;$i++) {
-//            echo "<p>$mo[$i]</p>";
-//        }
-//        ?>
-<!--    </div>-->
+
 </div>
 <a href="aufgaben.php">Aufgabenseite</a>
 <a href="formularst.php" >Bearbeiten</a>
