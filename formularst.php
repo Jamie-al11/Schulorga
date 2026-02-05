11bf0375c494302d8ce759353114cfb51b2c6d54
Jamie <jamie.al@outlook.de>
2026-02-05 11:11:37 +0100

    in die datenbank schreiben

--- formularst.php
+++ formularst.php
@@ -4,29 +4,23 @@
     <link href="styles/styles.css" rel="stylesheet"/>
 </head>
 
+<form>
+    <?php
+$pdo = new PDO('mysql:host=localhost;dbname=mein test', 'root', '');
+$statement = $pdo->query("SELECT * FROM `krasse tabelle`");
+while ($row = $statement->fetch()) {
+    echo $row['Jonas'];
+}
+$wert = "wert";
+$statement = $pdo->prepare("INSERT INTO `krasse tabelle` (`Jonas`) VALUES (:jonas)");
+$statement->execute([
+    ':jonas' => $wert
+]);
 
-<form method="post" action="empfangen.php">
-<h1>Schulorga</h1>
+echo "Eintrag gespeichert!";
+?>
 
-    <div class="stundenplan">
-        <?php
-        foreach (["mo" => "Montag", "di" => "Dienstag", "mi" => "Mittwoch", "do" => "Donnerstag", "fr" => "Freitag"] as $wochentagAbgekuerzt => $wochentagLang) {
-            echo '<div class="tag">';
-            echo "<h2>$wochentagLang</h2>";
-            echo "<hr>";
-            $x = 9;
-            $json = file_get_contents("test.txt");
-            $plan = json_decode($json, true);
-            $tag = $plan[$wochentagAbgekuerzt . "f"];
-            $n = sizeof($tag);
-            for ($i = 0; $i < $x; $i++) {
-                $value = array_key_exists($i,$tag) ? $tag[$i] : "";
-                echo "<p><input type='text' name='stundenplan[{$wochentagAbgekuerzt}f][]' value='$value'></p>";
-            }
-            echo "</div>";
-        }
-        ?>
-    </div>
-    <button type="submit"> Fertig </button>
+
+    ?>
 </form>
 <a href="index.php">Startseite</a>
