498b0c5faa5c9f9c79e40ad8d4ba0da4ff221a99
Jamie <jamie.al@outlook.de>
2026-02-06 16:48:27 +0100

    formular upgrad auf datenbank

--- formularst.php
+++ formularst.php
@@ -3,19 +3,40 @@
     <title>Schulorga</title>
     <link href="styles/styles.css" rel="stylesheet"/>
 </head>
+<?php
+function Select_fuer_die_Faecher($tagid,$stundenid,$aktuellerwert) {
 
-<form method="post" action="empfangen%20datenbank.php">
-    <?php
-$pdo = new PDO('mysql:host=localhost;dbname=mein test', 'root', '');
-$statement = $pdo->query("SELECT stunde.Stunde,fach.name FROM `stunde` JOIN `fach` ON fach.id = stunde.Fach_id; ");
-while ($row = $statement->fetch()) {
-    echo $row['name'];
-    echo "<p>";
+    $pdo = new PDO('mysql:host=localhost;dbname=mein test', 'root', '');
+    $statement = $pdo->query("SELECT id,name FROM `fach`");
+
+    echo "<select name='stundenplan[$tagid][$stundenid]'>";
+    foreach ($statement->fetchAll() as $row) {
+        echo "<option value='{$row['id']}' " . ($row['id'] == $aktuellerwert ? 'selected="selected"' : '') . ">{$row['name']}</option>";
+
+    }
+    echo "</select>";
 }
-echo "<input type=text name='row'>";
 ?>
 
+<form method="post" action="empfangen%20datenbank.php">
+<div class  = stundenplan >
+    <?php
+    foreach([ 'Montag','Dienstag','Mitwoch','Donnerstag','Freitag'] as $ganzername) {
+        echo '<div class="tag">';
+        echo "<h2>$ganzername</h2>";
+        echo "<hr>";
+        for ($i = 1; $i <= 9; $i++){
+            echo "<p>";
+            Select_fuer_die_Faecher($ganzername,$i,3);
+            echo "</p>";
+        }
+        echo "</div>";
+    }
+    ?>
 
+
+    </div>
     <button type="submit" >Fertig</button>
+
 </form>
 <a href="index.php">Startseite</a>
