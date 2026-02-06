d5b56bf96ed703085187e0d4911323c9cd732248
Jamie <jamie.al@outlook.de>
2026-02-06 10:20:44 +0100

    minor change kein änderung am code

--- empfangen.php
+++ empfangen.php
@@ -5,6 +5,7 @@ foreach (array_keys($stundenplan) as $key) {
     $stundenplan[$key] = array_filter($stundenplan[$key], fn($x) => $x !== "" and $x !== " ");
 }
 
+
 file_put_contents("test.txt", json_encode($stundenplan, JSON_PRETTY_PRINT));
 
 header("Location: http://localhost/Schulorga/");
