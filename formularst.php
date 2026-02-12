6e7660c6030cace9cdff091a56ab5114c4af33e6
Jamie <jamie.al@outlook.de>
2026-02-12 17:14:29 +0100

    bug fix für selected

--- formularst.php
+++ formularst.php
@@ -4,12 +4,8 @@
     <link href="styles/styles.css" rel="stylesheet"/>
 </head>
 <?php
-function Select_fuer_die_Faecher($tagid,$stundenid)
+function Select_fuer_die_Faecher($tagid,$stundenid,$faecher,$pdo)
 {
-    $pdo = new PDO('mysql:host=localhost;dbname=mein test', 'root', '');
-
-// Alle Fächer laden
-    $faecher = $pdo->query("SELECT id, name FROM fach")->fetchAll(PDO::FETCH_ASSOC);
 
 // Aktuelles Fach für diese Stunde laden
     $statement = $pdo->query("
@@ -21,14 +17,13 @@ function Select_fuer_die_Faecher($tagid,$stundenid)
       AND Stunde = '$stundenid'
 ");
 
-// Falls kein Eintrag existiert → null
     $aktuellesFach = $statement->fetch(PDO::FETCH_ASSOC)['fach_id'] ?? null;
 // Select ausgeben
     echo "<select name='stundenplan[$tagid][$stundenid]'>";
 
     foreach ($faecher as $fach) {
         $selected = ($fach['id'] == $aktuellesFach) ? "selected" : "";
-        echo "<option value='{$fach['id']}' $selected>{$fach['name']}</option>";
+        echo "<option value=" .$fach['id']." $selected>" . $fach['name']."</option>";
     }
 
     echo "</select>";
@@ -38,13 +33,17 @@ function Select_fuer_die_Faecher($tagid,$stundenid)
 <form method="post" action="empfangen%20datenbank.php">
 <div class  = stundenplan >
     <?php
+    $pdo = new PDO('mysql:host=localhost;dbname=mein test', 'root', '');
+
+    $faecher = $pdo->query("SELECT id, name FROM fach order by name")->fetchAll(PDO::FETCH_ASSOC);
+
     foreach(['Montag','Dienstag','Mittwoch', 'Donnerstag','Freitag'] as $ganzername) {
         echo '<div class="tag">';
         echo "<h2>$ganzername</h2>";
         echo "<hr>";
-        for ($i = 1; $i <= 6; $i++){
-            echo "<p>";
-            Select_fuer_die_Faecher($ganzername,$i);
+        for ($i = 1; $i <= 9; $i++){
+            echo "<p> $i";
+            Select_fuer_die_Faecher($ganzername,$i,$faecher,$pdo);
             echo "</p>";
         }
         echo "</div>";
